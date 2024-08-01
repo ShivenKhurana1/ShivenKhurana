b158f213201c1e045724e4b04b724281ccee3873
Shiven Khurana <shivenbk@gmail.com>
2024-08-01 15:33:08 -0400

    Update config.js

--- src/config.js
+++ src/config.js
@@ -8,19 +8,19 @@ module.exports = {
     },
     {
       name: 'Instagram',
-      url: 'https://www.instagram.com/bchiang7',
+      url: 'https://www.instagram.com/shiviwhivi',
     },
     {
-      name: 'Twitter',
-      url: 'https://twitter.com/bchiang7',
+      name: 'X or Twitter',
+      url: 'https://x.com/shivi_whivi',
     },
     {
       name: 'Linkedin',
-      url: 'https://www.linkedin.com/in/bchiang7',
+      url: '',
     },
     {
       name: 'Codepen',
-      url: 'https://codepen.io/bchiang7',
+      url: 'https://codepen.io/ShiviWhivi',
     },
   ],
 
