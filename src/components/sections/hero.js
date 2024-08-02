0b779176465c92000d979265b543cdd0760653ab
Shiven Khurana <shivenbk@gmail.com>
2024-08-01 22:07:25 -0400

    Update hero.js

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -60,28 +60,23 @@ const Hero = () => {
   }, []);
 
   const one = <h1>Hi, my name is</h1>;
-  const two = <h2 className="big-heading">Brittany Chiang.</h2>;
-  const three = <h3 className="big-heading">I build things for the web.</h3>;
+  const two = <h2 className="big-heading">Shiven Khurana.</h2>;
+  const three = <h3 className="big-heading">I build things for the public, with the help of AI.</h3>;
   const four = (
     <>
       <p>
-        I’m a software engineer specializing in building (and occasionally designing) exceptional
-        digital experiences. Currently, I’m focused on building accessible, human-centered products
-        at{' '}
-        <a href="https://upstatement.com/" target="_blank" rel="noreferrer">
-          Upstatement
-        </a>
-        .
+        I’m a software engineer, math enthusiast, and a computer scientist, specializing in building (and occasionally designing) exceptional
+        digital experiences. Currently, I’m focused on building accessible, human-centered products.
       </p>
     </>
   );
   const five = (
     <a
       className="email-link"
-      href="https://www.newline.co/courses/build-a-spotify-connected-app"
+      href="https://drive.google.com/file/d/1XAYTyYfqJ3YOELc0OIFiTLTVoRNHH8mQ/view"
       target="_blank"
       rel="noreferrer">
-      Check out my course!
+      Check out my latest research paper!
     </a>
   );
 
